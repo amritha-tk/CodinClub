63ca752b1cc726a48c6be5ff30ea4318491909b2
amritha-tk <amrithatk95@gmail.com>
2020-06-02 19:36:58 +0530

    Committed by modifying with some condition

--- Employeeatt.sh
+++ Employeeatt.sh
@@ -6,19 +6,25 @@ echo "Welcome to Employee Wage Computation Program"
 isPartTime=1;
 isFullTime=2;
 Maxrateperhr=20;
-Maxworkingday=20;
+Maxworkingdays=20;
 empCheck=$((RANDOM%3))
 
+totalworkingdays=0;
+
 case $empCheck in
-$isPartTime) empHrs=4;;
-$isFullTime) empHrs=8;;
-*) empHrs=0;;
+ $isPartTime) empHrs=4;;
+ $isFullTime) empHrs=8;;
+ *) empHrs=0;;
 esac
 
-salary=$(($empHrs*$Maxrateperhr))
-wageinMon=$(($salary*$Maxworkingday))
-echo "Salary is "$salary
-echo "Salary in month" $wageinMon
+while [[ $totalworkingdays -lt $Maxworkingdays ]]
+ do
+((totalworkingdays++))
+  salary=$(( $empHrs * $Maxrateperhr ))
+  wageinMon=$(( $salary * $Maxworkingdays ))
+done
+  echo "Salary is "$salary
+  echo "Salary in month" $wageinMon
 
 #Check Employee present or absent
  attendance=$(( RANDOM%1 +1))
