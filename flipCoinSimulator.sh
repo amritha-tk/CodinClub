0637bbf06bfeb6c8055c44c08c3cefe74cb8de37
amritha-tk <amrithatk95@gmail.com>
2020-06-05 22:38:01 +0530

    Simulated a coin flip using loop

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,6 +1,9 @@
 
 #!/bin/bash -x
 
+read -p "Enter number of times to flip the coin : " n
+for (( i=0; i<$n; i++ ))
+do
 rand=$(( (RANDOM %2 ) + 1 ))
 if [ $rand -eq 1 ]
  then
@@ -8,3 +11,4 @@ if [ $rand -eq 1 ]
 else
   echo "...Tails.."
 fi
+done
