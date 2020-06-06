911c7571d15a7c6df5615bcc2662e6d6ab05cc88
amritha-tk <amrithatk95@gmail.com>
2020-06-06 21:10:17 +0530

    Added coin combination UC4

--- Coincombo.sh
+++ Coincombo.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 
 declare -A result1
 read -p "Enter how many times to iterate" n
@@ -89,7 +89,7 @@ done
 echo The win percentage in doublet coin toss
 for i in ${!result2[@]}
 do
-precentage=$( echo ${result2[$i]} $n | awk '{print $1/$2 * 100}' )
+percentage=$( echo ${result2[$i]} $n | awk '{print $1/$2 * 100}' )
 echo "$i => $percentage%"
 done
 
@@ -103,4 +103,109 @@ do
     fi
 done
 echo The winner in doublet toss  $max2
+
+declare -A result3
+echo "The Triple coin toss"
+for (( count=0; count<$n; count++ ))
+ do
+   rand1=$((RANDOM%2))
+   rand2=$((RANDOM%2))
+   rand3=$((RANDOM%2))
+   if [[ $rand1 -eq 1 ]]
+    then
+         if [[ $rand2 -eq 1 ]]
+           then
+             if [[ $rand3 -eq 1 ]]
+               then
+                if [[ ${result3["HHH"]} -eq null ]]
+                 then
+                  result3["HHH"]=1
+                 else
+                   result3["HHH"]=$((${result3["HHH"]}+1))
+                 fi
+
+             else
+                if [[ ${result3["HHT"]} -eq null ]]
+                  then
+                    result3["HHT"]=1
+                 else
+                     result3["HHT"]=$((${result3["HHT"]}+1))
+                 fi
+              fi
+          else
+            if [[ $rand3 -eq 1 ]]
+             then
+               if [[ ${result3["HTH"]} -eq null ]]
+                 then
+                   result3["HTH"]=1
+                  else
+                    result3["HTH"]=$((${result3["HTH"]}+1))
+               fi
+              else
+                 if [[ ${result3["HTT"]} -eq null ]]
+                   then
+                     result3["HTT"]=1
+                    else
+                     result3["HTT"]=$((${result3["HTT"]}+1))
+                 fi
+             fi
+         fi
+    
+ if [[ $rand2 -eq 1 ]]
+           then
+             if [[ $rand3 -eq 1 ]]
+               then
+                if [[ ${result3["TTH"]} -eq null ]]
+                 then
+                  result3["TTH"]=1
+                 else
+                   result3["TTH"]=$((${result3["TTH"]}+1))
+                 fi
+
+             else
+                if [[ ${result3["THT"]} -eq null ]]
+                  then
+                    result3["THT"]=1
+                 else
+                     result3["THT"]=$((${result3["THT"]}+1))
+                 fi
+              fi
+          else
+            if [[ $rand3 -eq 1 ]]
+             then
+               if [[ ${result3["THH"]} -eq null ]]
+                 then
+                   result3["THH"]=1
+                  else
+                    result3["THH"]=$((${result3["THH"]}+1))
+               fi
+              else
+                 if [[ ${result3["TTT"]} -eq null ]]
+                   then
+                     result3["TTT"]=1
+                    else
+                     result3["TTT"]=$((${result3["TTT"]}+1))
+                 fi
+             fi
+         fi
+    fi
+done
+
+echo The percentage win for triplet coin flip
+for i in ${!result3[@]}
+do
+percentage=$( echo ${result3[$i]} $n | awk '{print $1/$2 * 100}' )
+echo "$i => $percentage%"
+done
+
+max3="HHH"
+
+for i in ${!result3[@]}
+do
+ if [[ $result3[$i]} -gt  ${result3[$max3]} ]]
+  then
+       max3=$i
+    fi
+done
+echo The winner in triplet toss  $max3
 
